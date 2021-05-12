d34723db69ed1be836e784be4d32a96e2db24a9f
Christof Petig <christof.petig@arcor.de>
2021-05-12 22:39:14 +0200

    fully wrapping

--- test2.rs
+++ test2.rs
@@ -1,35 +1,51 @@
 
-pub struct A {
+pub trait DB {
+    fn action(&mut self, x: i32);
 }
 
-pub struct B<'a> {
-    a: &'a mut A,
+pub struct Base {
 }
 
-impl A {
-    pub fn scope<'b, 'a: 'b>(&'a mut self) -> B<'b> {
-        B{a: self}
+pub struct Wrapper<'a, X: DB> (&'a mut X);
+
+impl Base {
+    pub fn scope<'a>(&'a mut self) -> Wrapper<'a,Base> {
+        Wrapper(self)
     }
     pub fn new() -> Self {
         Self{}
     }
-    pub fn action(x: i32) {
+}
+
+impl DB for Base {
+    fn action(&mut self, x: i32) {
         println!("{}", x);
     }
 }
 
-impl<'b, 'a: 'b> B<'a> {
-    pub fn scope(&'a mut self) -> B<'b> {
-        B{a: self.a}
+impl<'b, 'a: 'b, X:DB> Wrapper<'a, X> {
+    pub fn scope(&'a mut self) -> Wrapper<'b, Wrapper<'a, X>> {
+        Wrapper(self)
+    }
+}
+
+impl<'a, X:DB> DB for Wrapper<'a, X> {
+    fn action(&mut self, x: i32) {
+        print!(".");
+        self.0.action(x);
     }
 }
 
 fn main() {
-    let mut a = A::new();
-    for i in 0..4 {
+    let mut a = Base::new();
+    a.action(0);
+    for i in 1..5 {
         let mut b = a.scope();
-        for j in 0..4 {
+        b.action(i);
+        for j in 0..4 
+        {
             let mut c = b.scope();
+            c.action(5+j);
         }
     }
 }
