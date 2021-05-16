310d0f13af9f667e264904f9037b0bec5bd00e18
Christof Petig <christof.petig@arcor.de>
2021-05-16 12:41:14 +0200

    attempt (difficult to get right)

--- test2.rs
+++ test2.rs
@@ -24,7 +24,7 @@ impl DB for Base {
 }
 
 impl<'b, 'a: 'b, X:DB> Wrapper<'a, X> {
-    pub fn scope(&'a mut self) -> Wrapper<'b, Wrapper<'a, X>> {
+    pub fn scope(&'b mut self) -> Wrapper<'b, Wrapper<'a, X>> {
         Wrapper(self)
     }
 }
@@ -36,16 +36,25 @@ impl<'a, X:DB> DB for Wrapper<'a, X> {
     }
 }
 
+fn sub<'b, 'a : 'b, X: DB>(b: &'a mut Wrapper<'b, X>) {
+    let j=1;
+    let mut c = b.scope();
+    c.action(5+j);
+}
+
 fn main() {
     let mut a = Base::new();
     a.action(0);
     for i in 1..5 {
         let mut b = a.scope();
         b.action(i);
-        for j in 0..4 
-        {
-            let mut c = b.scope();
-            c.action(5+j);
-        }
+        let x=&mut b;
+        sub(x);
+        sub(x);
+        // for j in 0..4 
+        // {
+        //     let mut c = b.scope();
+        //     c.action(5+j);
+        // }
     }
 }
